8f1c808abf978530e71c70b0e696001e7787b918
Elías Sauthier <elias.sauthier@compasso.com.br>
2021-02-28 18:18:00 -0300

    aula 3: regra de negócio -> experienceBar e challengesCompleted

--- src/contexts/ChallengesContext.tsx
+++ src/contexts/ChallengesContext.tsx
@@ -12,6 +12,7 @@ interface ChallengesContextData {
   currentExperience: number;
   challengesCompleted: number;
   activeChallenge: Challenge;
+  experienceToNextLevel: number;
   levelUp: () => void;
   startNewChallenge: () => void;
   resetChallenge: () => void;
@@ -29,6 +30,8 @@ export function ChallengesProvider({ children } : ChallengesProviderProps) {
   const [challengesCompleted, setChallengesCompleted] = useState(0);
 
   const [activeChallenge, setActiveChallenge] = useState(null);
+
+  const experienceToNextLevel = Math.pow((level + 1) * 4, 2);
 
   function levelUp() {
     setLevel(level + 1);
@@ -54,7 +57,8 @@ export function ChallengesProvider({ children } : ChallengesProviderProps) {
         levelUp,
         startNewChallenge,
         activeChallenge,
-        resetChallenge
+        resetChallenge,
+        experienceToNextLevel
       }}
     >
       { children }
