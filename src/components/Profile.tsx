442c365d6d1139702c7bd8e6a82c206c12ab9e0a
Elías Sauthier <elias.sauthier@compasso.com.br>
2021-02-28 19:45:40 -0300

    aula 4: leveling -> ao clicar em 'Completei', atualizar level, xp e desafios completos

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,6 +1,11 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
+
 import styles from '../styles/components/Profile.module.css';
 
 export function Profile () {
+  const { level } = useContext(ChallengesContext);
+
   return (
     <div className={styles.profileContainer}>
       <img src="https://avatars.githubusercontent.com/u/18060247" alt="Imagem do perfil de Elías"/>
@@ -8,7 +13,7 @@ export function Profile () {
         <strong>Elías</strong>
         <p>
           <img src="icons/level.svg" alt="ícone de level"/>
-          Level 1</p>
+          Level {level}</p>
       </div>
     </div>
   );
