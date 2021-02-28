937b8a713a4779bf06dfef0db440d0513f5cc66d
Elías Sauthier <elias.sauthier@compasso.com.br>
2021-02-28 18:02:37 -0300

    aula 3: regra de negócio -> ao finalizar um ciclo (timer zerar) exibir um desafio; ao clicar em falhei no desafio exibido, substituir o desafio atual pela tela de desafio/challenge inicial

--- src/contexts/ChallengesContext.tsx
+++ src/contexts/ChallengesContext.tsx
@@ -1,11 +1,20 @@
 import { createContext, useState, ReactNode } from 'react';
+import challenges from '../../challenges.json';
+
+interface Challenge {
+  type: 'body' | 'eye';
+  description: string;
+  amount: number;
+}
 
 interface ChallengesContextData {
   level: number;
   currentExperience: number;
   challengesCompleted: number;
+  activeChallenge: Challenge;
   levelUp: () => void;
   startNewChallenge: () => void;
+  resetChallenge: () => void;
 }
 
 interface ChallengesProviderProps {
@@ -19,18 +28,34 @@ export function ChallengesProvider({ children } : ChallengesProviderProps) {
   const [currentExperience, setCurrentExperience] = useState(0);
   const [challengesCompleted, setChallengesCompleted] = useState(0);
 
+  const [activeChallenge, setActiveChallenge] = useState(null);
+
   function levelUp() {
     setLevel(level + 1);
   }
 
   function startNewChallenge() {
-    console.log('new challenge');
-    
+    const randomChallengeIndex = Math.floor(Math.random() * challenges.length);
+    const challenge = challenges[randomChallengeIndex];
+
+    setActiveChallenge(challenge);
+  }
+
+  function resetChallenge() {
+    setActiveChallenge(null);
   }
 
   return (
     <ChallengesContext.Provider 
-      value={{ level, currentExperience, challengesCompleted, levelUp, startNewChallenge }}
+      value={{ 
+        level,
+        currentExperience,
+        challengesCompleted,
+        levelUp,
+        startNewChallenge,
+        activeChallenge,
+        resetChallenge
+      }}
     >
       { children }
     </ChallengesContext.Provider>
